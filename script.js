0834a30b6dc1402ed735f0aba9dc6a65ba5ba7b8
draby2466 <ribhavyadav2466@gmail.com>
2025-01-07 21:39:34 +0530

    Moved the slider background calculation to separate function.

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 // Get the grid size from the slider
 let slider = document.getElementById("slider");
-let boxesPerSide = document.querySelector('.grid');
-boxesPerSide.textContent = slider.value;
-slider.addEventListener('input', (e) => {
-    let valuePercentage = (slider.value / slider.max) * 100;
+let value = document.querySelector('.grid');
+value.textContent = slider.value;
+
+function calcSliderValue() {
+    let valuePercentage = (slider.value / slider.max) * (100);
     slider.style.background = `linear-gradient(to right, black ${valuePercentage}%, #f5f5f5 ${valuePercentage}%)`;
-    boxesPerSide.textContent = `${e.target.value} x ${e.target.value}`;
+}
+calcSliderValue()
+
+slider.addEventListener('input', (e) => {
+    calcSliderValue();
+    value.textContent = `${e.target.value} x ${e.target.value}`;
 })
 
 // Add 16x16 boxes to the canvas
 const canvas = document.querySelector('.canvas');
-for (let i = 0; i < boxesPerSide * boxesPerSide; i++) {
+for (let i = 0; i < 16 * 16; i++) {
     const div = document.createElement('div');
     div.classList.add('box');
     canvas.appendChild(div);
@@ -18,6 +24,7 @@ for (let i = 0; i < boxesPerSide * boxesPerSide; i++) {
 
 // Add event listeners to color the boxes
 let isDrawing = false;
+
 function colorBox(e) {
     if (isDrawing && e.target.classList.contains('box')) {
         e.target.style.backgroundColor = 'blue';
